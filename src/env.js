15d250a19aff9e349d69761100ba0b8ff21ff738
Benjamin Zambelli <benjacxy@gmail.com>
2023-06-23 00:07:17 -0300

    Update backend

--- src/env.js
+++ src/env.js
@@ -1,4 +1,6 @@
 export const smtpServer = import.meta.env.VITE_SMTP_SERVER;
 export const smtpPort = import.meta.env.VITE_SMTP_PORT;
 export const senderEmail = import.meta.env.VITE_SENDER_EMAIL;
-export const receiverEmail = import.meta
+export const receiverEmail = import.meta.env.VITE_RECEIVER_EMAIL;
+export const smtpUsername = import.meta.env.VITE_SMTP_USERNAME;
+export const smtpPassword = import.meta.env.VITE_SMTP_PASSWORD;
